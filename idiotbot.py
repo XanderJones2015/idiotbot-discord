6883fdc50ede60726e4d5f73ac000393bc5062fd
Bluecode1 <xanderjones2015@gmail.com>
2017-12-05 23:14:24 +0000

    fixed

--- idiotbot.py
+++ idiotbot.py
@@ -14,9 +14,5 @@ async def idiot(ctx, user: discord.Member):
 async def idiotm(ctx):
     await bot.say("Michael you idiot")
 
-<<<<<<< HEAD
 bot.run("")
-=======
-bot.command
-bot.run("Mzg1OTY0Njk3MTI5NTgyNTky.DQJBKQ.UEICEHQXmqOQ4CAYgMW4pkBbepA")
->>>>>>> 3f59ec1f2cdf2dfa2a37ef6bb1f3da9a29612ab9
+
